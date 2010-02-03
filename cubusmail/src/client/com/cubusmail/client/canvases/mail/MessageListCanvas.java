31b27f2183a065adb4f9ff7c9ca85c32e6437618
juergen@schlierf.eu <juergen@schlierf.eu@fdd5f79a-79e0-11de-888d-2103c0e6b660>
2010-02-03 15:46:56 +0000

    SmartGWT migration

--- cubusmail/src/client/com/cubusmail/client/canvases/mail/MessageListCanvas.java
+++ cubusmail/src/client/com/cubusmail/client/canvases/mail/MessageListCanvas.java
@@ -30,13 +30,17 @@ import com.cubusmail.common.model.GWTMailConstants;
 import com.cubusmail.common.model.GWTMailFolder;
 import com.cubusmail.common.model.GWTMessage;
 import com.cubusmail.common.model.GWTMessageRecord;
-import com.cubusmail.common.model.ImageProvider;
 import com.cubusmail.common.model.MessageListFields;
 import com.smartgwt.client.data.Criteria;
 import com.smartgwt.client.data.RecordList;
-import com.smartgwt.client.widgets.Button;
 import com.smartgwt.client.widgets.form.DynamicForm;
+import com.smartgwt.client.widgets.form.fields.FormItemIcon;
+import com.smartgwt.client.widgets.form.fields.PickerIcon;
 import com.smartgwt.client.widgets.form.fields.TextItem;
+import com.smartgwt.client.widgets.form.fields.events.IconClickEvent;
+import com.smartgwt.client.widgets.form.fields.events.IconClickHandler;
+import com.smartgwt.client.widgets.form.fields.events.KeyPressEvent;
+import com.smartgwt.client.widgets.form.fields.events.KeyPressHandler;
 import com.smartgwt.client.widgets.layout.SectionStack;
 import com.smartgwt.client.widgets.layout.SectionStackSection;
 import com.smartgwt.client.widgets.layout.VLayout;
@@ -69,14 +73,35 @@ public class MessageListCanvas extends VLayout implements MessagesReloadListener
 
 		this.searchItem = new TextItem();
 		this.searchItem.setTitle( "Search" );
-		DynamicForm searchCanvas = new DynamicForm();
-		searchCanvas.setItems( this.searchItem );
+		this.searchItem.setWidth( 200 );
+		DynamicForm searchForm = new DynamicForm();
+		searchForm.setItems( this.searchItem );
 
-		Button searchButton = new Button( "" );
-		searchButton.setBorder( "0px" );
-		searchButton.setIcon( ImageProvider.FIND );
-		searchButton.setAutoFit( true );
-		section.setControls( searchCanvas, searchButton );
+		final PickerIcon searchIcon = new PickerIcon( PickerIcon.SEARCH );
+		final PickerIcon clearIcon = new PickerIcon( PickerIcon.CLEAR );
+		this.searchItem.setIcons( searchIcon, clearIcon );
+		this.searchItem.addIconClickHandler( new IconClickHandler() {
+
+			public void onIconClick( IconClickEvent event ) {
+
+				FormItemIcon icon = event.getIcon();
+				if ( icon.getSrc().equals( clearIcon.getSrc() ) ) {
+					resetFilter();
+				}
+				loadMessages();
+			}
+		} );
+		this.searchItem.addKeyPressHandler( new KeyPressHandler() {
+
+			public void onKeyPress( KeyPressEvent event ) {
+
+				if ( "enter".equalsIgnoreCase( event.getKeyName() ) ) {
+					loadMessages();
+				}
+			}
+		} );
+
+		section.setControls( searchForm );
 
 		this.grid = new MessageListGrid();
 		this.section.setItems( this.grid );
@@ -98,7 +123,6 @@ public class MessageListCanvas extends VLayout implements MessagesReloadListener
 		EventBroker.get().addMessageLoadedListener( this );
 	}
 
-
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -107,10 +131,8 @@ public class MessageListCanvas extends VLayout implements MessagesReloadListener
 	 */
 	public void onMessagesReload() {
 
-		Criteria criteria = new Criteria( GWTMailConstants.PARAM_FOLDER_ID, GWTSessionManager.get()
-				.getCurrentMailFolder().getId() );
-		this.grid.invalidateCache();
-		this.grid.fetchData( criteria );
+		resetFilter();
+		loadMessages();
 	}
 
 	/*
@@ -122,11 +144,28 @@ public class MessageListCanvas extends VLayout implements MessagesReloadListener
 	 */
 	public void onFolderSelected( GWTMailFolder mailFolder ) {
 
-		// TODO: set the right section style
+		resetFilter();
 		this.sectionStack.setSectionTitle( 0, "&nbsp;&nbsp;" + mailFolder.getName() );
-		Criteria criteria = new Criteria( GWTMailConstants.PARAM_FOLDER_ID, mailFolder.getId() );
+		loadMessages();
+	}
+
+	/**
+	 * @param criteria
+	 */
+	private void loadMessages() {
+
+		Criteria criteria = new Criteria( GWTMailConstants.PARAM_FOLDER_ID, GWTSessionManager.get()
+				.getCurrentMailFolder().getId() );
 		this.grid.invalidateCache();
 		this.grid.fetchData( criteria );
+	}
+
+	/**
+	 * 
+	 */
+	private void resetFilter() {
+
+		this.searchItem.clearValue();
 	}
 
 	/*
@@ -140,15 +179,14 @@ public class MessageListCanvas extends VLayout implements MessagesReloadListener
 
 		RecordList list = this.grid.getRecordList();
 		if ( list != null && list.getLength() > 0 ) {
-			MessageGridRecord record = (MessageGridRecord) list.find( MessageListFields.ID.name(), String.valueOf( message
-					.getId() ) );
+			MessageGridRecord record = (MessageGridRecord) list.find( MessageListFields.ID.name(), String
+					.valueOf( message.getId() ) );
 			if ( record != null ) {
 				GWTMessageRecord source = message.getMessageRecord();
 				int index = this.grid.getRecordIndex( record );
 				if ( index > -1 ) {
 					record.setGWTMessageRecord( source );
 					this.grid.refreshRow( index );
-					// this.grid.redraw();
 				}
 			}
 		}
